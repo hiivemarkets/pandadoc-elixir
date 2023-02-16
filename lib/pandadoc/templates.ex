a1ee54fd51b7091b5bb46a3e158367173f9f0a50
Ilya Suzdalnitski <ilya@suzdalnitski.com>
2023-02-15 20:37:39 -0500

    feature: template details

---
+++ lib/pandadoc/templates.ex
@@ -0,0 +1,24 @@
+defmodule Pandadoc.Templates do
+  @moduledoc """
+  The `Pandadoc.Templates` module provides access methods to the
+  All methods require a Tesla Client struct created with `Pandadoc.client(api_key)`.
+
+  ## Examples
+
+      client = Pandadoc.Client("thisismykeycreatedinpandadoc")
+      {:ok, result, _env} = Pandadoc.Templates.template_details(client, "template_id")
+  """
+
+  @templates_url "/templates"
+
+  @doc """
+  Return detailed data about a template
+
+  https://developers.pandadoc.com/reference/template-details
+  """
+  @spec template_details(Pandadoc.client(), String.t()) :: Pandadoc.result()
+  def template_details(client, template_id) do
+    Tesla.get(client, @templates_url <> "/#{template_id}/details")
+    |> Pandadoc.result()
+  end
+end
