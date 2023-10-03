da5333f4dc0e464e521ef0ae07132a5d86c2ffaf
Sean Huggins <seanhugg@gmail.com>
2023-10-03 13:36:12 -0700

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pandadoc.MixProject do
   def project do
     [
       app: :pandadoc_api,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
